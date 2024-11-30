070c44cc1470137833d5995ea46b96573ef5cc03
Denis <mikulaj.d@gmail.com>
2024-11-30 18:06:15 +0100

    Zadanie 1- Update podla review*

--- plugins/applogger/logger/Plugin.php
+++ plugins/applogger/logger/Plugin.php
@@ -1,4 +1,6 @@
-<?php namespace AppLogger\Logger;
+<?php
+
+namespace AppLogger\Logger;
 
 use Backend;
 use System\Classes\PluginBase;
@@ -24,67 +26,6 @@ class Plugin extends PluginBase
             'description' => 'No description provided yet...',
             'author' => 'AppLogger',
             'icon' => 'icon-leaf'
-        ];
-    }
-
-    /**
-     * register method, called when the plugin is first registered.
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * boot method, called right before the request route.
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * registerComponents used by the frontend.
-     */
-    public function registerComponents()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'AppLogger\Logger\Components\MyComponent' => 'myComponent',
-        ];
-    }
-
-    /**
-     * registerPermissions used by the backend.
-     */
-    public function registerPermissions()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'applogger.logger.some_permission' => [
-                'tab' => 'Logger',
-                'label' => 'Some permission'
-            ],
-        ];
-    }
-
-    /**
-     * registerNavigation used by the backend.
-     */
-    public function registerNavigation()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'logger' => [
-                'label' => 'Logger',
-                'url' => Backend::url('applogger/logger/mycontroller'),
-                'icon' => 'icon-leaf',
-                'permissions' => ['applogger.logger.*'],
-                'order' => 500,
-            ],
         ];
     }
 }
